4b1effe463efc66193832a4593fb0cfd2c97ff35
ola4paradowska-tech <ola4paradowska@gmail.com>
2026-01-31 12:42:08 +0100

    push

--- dane.py
+++ dane.py
@@ -7,8 +7,11 @@ from PyQt6.QtWidgets import (
     QTableWidget,
     QTableWidgetItem,
     QVBoxLayout,
-    QMessageBox
+    QMessageBox,
+    QComboBox,
+    QHBoxLayout
 )
+from PyQt6.QtCore import Qt
 
 class CsvTableViewer(QWidget):
     def __init__(self):
@@ -19,11 +22,24 @@ class CsvTableViewer(QWidget):
         self.button = QPushButton("Wczytaj dane")
         self.button.clicked.connect(self.load_csv)
 
+        self.column_select = QComboBox()
+        self.order_select = QComboBox()
+        self.order_select.addItems(["Rosnąco", "Malejąco"])
+
+        self.sort_button = QPushButton("Sortuj")
+        self.sort_button.clicked.connect(self.sort_table)
+
+        sort_layout = QHBoxLayout()
+        sort_layout.addWidget(self.column_select)
+        sort_layout.addWidget(self.order_select)
+        sort_layout.addWidget(self.sort_button)
+
         self.table = QTableWidget()
         self.table.setAlternatingRowColors(True)
 
         layout = QVBoxLayout()
         layout.addWidget(self.button)
+        layout.addLayout(sort_layout)  # ⬅ TU
         layout.addWidget(self.table)
         self.setLayout(layout)
 
@@ -39,6 +55,8 @@ class CsvTableViewer(QWidget):
             self.table.setColumnCount(len(headers))
             self.table.setRowCount(len(rows))
             self.table.setHorizontalHeaderLabels(headers)
+            self.column_select.clear()
+            self.column_select.addItems(headers)
 
             for row_idx, row in enumerate(rows):
                 for col_idx, value in enumerate(row):
@@ -52,6 +70,18 @@ class CsvTableViewer(QWidget):
 
         except Exception as e:
             QMessageBox.critical(self, "Błąd", str(e))
+
+    def sort_table(self):
+        col_index = self.column_select.currentIndex()
+        order_text = self.order_select.currentText()
+
+        order = (
+            Qt.SortOrder.AscendingOrder
+            if order_text == "Rosnąco"
+            else Qt.SortOrder.DescendingOrder
+        )
+
+        self.table.sortItems(col_index, order)
 
 
 if __name__ == "__main__":
