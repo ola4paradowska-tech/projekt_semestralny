382a673ba1703dbebde82044059f09d5bf5ed29d
ola4paradowska-tech <ola4paradowska@gmail.com>
2026-01-31 12:25:04 +0100

    push

--- dane.py
+++ dane.py
@@ -1,51 +1,63 @@
 import pandas as pd
 
 import sys
+import csv
 from PyQt6.QtWidgets import (
     QApplication,
     QWidget,
     QPushButton,
-    QTextEdit,
+    QTableWidget,
+    QTableWidgetItem,
     QVBoxLayout,
     QMessageBox
 )
 
-class CsvViewer(QWidget):
+class CsvTableViewer(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Podgląd CSV")
-        self.resize(800, 600)
+        self.setWindowTitle("Dane pacjentów")
+        self.resize(900, 600)
 
         self.button = QPushButton("Wczytaj dane")
         self.button.clicked.connect(self.load_csv)
 
-        self.text_area = QTextEdit()
-        self.text_area.setReadOnly(True)
+        self.table = QTableWidget()
+        self.table.setAlternatingRowColors(True)
 
         layout = QVBoxLayout()
         layout.addWidget(self.button)
-        layout.addWidget(self.text_area)
+        layout.addWidget(self.table)
         self.setLayout(layout)
 
     def load_csv(self):
         try:
-            with open("patients_data_v3.csv", "r", encoding="utf-8") as f:
-                self.text_area.setPlainText(f.read())
-        except FileNotFoundError:
-            QMessageBox.critical(
-                self,
-                "Błąd",
-                "Nie znaleziono pliku patients_data_v3.csv"
-            )
+            with open("patients_data_v3.csv", newline="", encoding="utf-8") as file:
+                reader = csv.reader(file)
+                data = list(reader)
+
+            headers = data[0]
+            rows = data[1:]
+
+            self.table.setColumnCount(len(headers))
+            self.table.setRowCount(len(rows))
+            self.table.setHorizontalHeaderLabels(headers)
+
+            for row_idx, row in enumerate(rows):
+                for col_idx, value in enumerate(row):
+                    self.table.setItem(
+                        row_idx,
+                        col_idx,
+                        QTableWidgetItem(value)
+                    )
+
+            self.table.resizeColumnsToContents()
+
         except Exception as e:
-            QMessageBox.critical(
-                self,
-                "Błąd",
-                str(e)
-            )
+            QMessageBox.critical(self, "Błąd", str(e))
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = CsvViewer()
+    window = CsvTableViewer()
     window.show()
     sys.exit(app.exec())
